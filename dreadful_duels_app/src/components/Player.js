bd2d29449fc3448649fee57c6598b1f9dc51dc28
Kagke <kagkelisg@gmail.com>
2023-09-12 13:16:39 +0300

    added player input name and choose enemy dropdown

--- dreadful_duels_app/src/components/Player.js
+++ dreadful_duels_app/src/components/Player.js
@@ -1,6 +1,5 @@
 function Player({score, name}){
      
-     const legends = ["Porko The Tamer", "Kagke The War Troll", "Aurelijon From The Cosmic Dawn", "Roberto Of The 7 Seas", "Rainer The Paladin", "Yismin From The Shadow Isles"]
      
      return<>
      <div>
